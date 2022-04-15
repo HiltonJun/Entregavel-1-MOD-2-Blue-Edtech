30b174926e08ded65575ff73c7f9f0c1bde10965
HiltonJun <hilton.honda@gmail.com>
2022-04-14 23:11:05 -0300

    Aula 01: Estruturação de rotas, criação do Html, CSS básico e script

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+
 const port = 3000;
 const app = express();
-const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
 const burgers = [
   {
-    id: "1",
-    nome: "Voando Alto",
-    descricao:
-      "Pão brioche/caseirinho, burguer de fraldinha 150g (fraldinha), alface, cebola caramelizada, queijo prato e maionese temperada (alho e temperos frescos)",
-    foto: "/assets/images/voando-alto.jpg",
-    preco: "R$27,90",
+    id: 1,
+    nome: 'Voando Alto',
+    descricao: 'Pão brioche/caseirinho, burguer de fraldinha 150g (fraldinha), alface, cebola caramelizada, queijo prato e maionese temperada (alho e temperos frescos).',
+    foto: '/assets/images/voando-alto.jpg',
+    preco: 27.90,
   },
   {
-    id: "2",
-    nome: "Espetacular Cheddar",
-    descricao:
-      "Pão australiano, hambúrguer artesanal de costela 150g, cheddar cremoso, cebola caramelizada e bacon",
-    foto: "/assets/images/blitz-cheddar.jpg",
-    preco: "R$32,90",
+    id: 2,
+    nome: 'Espetacular Cheddar',
+    descricao: 'Pão australiano, hambúrguer artesanal de costela 150g, cheddar cremoso, cebola caramelizada e bacon.',
+    foto: '/assets/images/blitz-cheddar.jpg',
+    preco: 32.90,
   },
   {
-    id: "3",
-    nome: "Marilú chicken",
-    descricao:
-      "Pão caseiro , um incrível e suculento hambúrguer de frango, alface, tomate, cebola caramelizada, queijo e maionese de limão siciliano.",
-    foto: "/assets/images/marilu-chicken.jpg",
-    preco: "R$27,90",
+    id: 3,
+    nome: 'Marilú chicken',
+    descricao: 'Pão caseiro , um incrível e suculento hambúrguer de frango, alface, tomate, cebola caramelizada, queijo e maionese de limão siciliano.',
+    foto: '/assets/images/marilu-chicken.jpg',
+    preco: 27.90,
   },
 ];
-app.get("/", (req, res) => {
-  res.send("index");
+
+app.get('/', function (req, res) {
+  res.send('Hello world');
 });
 
-app.get("/burgers/find-burgers", (req, res) => {
+app.get('/burgers/find-burgers', (req, res) => {
   res.send(burgers);
 });
 
-app.get("/burgers/find-burgers/:id", (req, res) => {
+app.get('/burgers/find-burgers/:id', (req, res) => {
   const idParam = req.params.id;
-  const selectburger = burguers.find((burger) => burger.id == idParam);
-  res.send(selectburger);
+  const chosenBurger = burgers.find((burger) => burger.id == idParam);
+  res.send(chosenBurger);
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
--- script.js
+++ script.js
@@ -1,47 +1,51 @@
-const baseUrl = "http://localhost:3000/burgers";
+const baseUrl = 'http://localhost:3000/burgers';
 
 async function findAllBurgers() {
   const response = await fetch(`${baseUrl}/find-burgers`);
-
   const burgers = await response.json();
+
   burgers.forEach((burger) => {
-    document.getElementById("burgerList").insertAdjacentHTML(
-      "beforeend",
-      `<div class="burgerListaItem">
+    document.getElementById('burgerList').insertAdjacentHTML(
+      'beforeend',
+      `<div class="BurgerListaItem">
         <div>
-            <div class="burgerListaItem-nome">${burger.nome}</div>
-            <div class="burgerListaItem-preco">R$ ${burger.preco.toFixed(
-              2
-            )}</div>
-            <div class="burgerListaItem-descricao">${burger.descricao}</div>
-          </div>
-            <img class="burgerListaItem-foto" src=${
-              paleta.foto
-            } alt=${`${burger.nome}`} />
-        </div>`
+          <div class="BurgerListaItem__nome">${burger.nome}</div>
+          <div class="BurgerListaItem__descricao">${burger.descricao}</div>
+          <div class="BurgerListaItem__preco">R$ ${burger.preco.toFixed(
+            2,
+          )}</div>
+        </div>
+          <img class="BurgerListaItem__foto" src=${
+            burger.foto
+          } alt=${`${burger.nome}`} />
+      </div>`,
     );
   });
 }
 
 findAllBurgers();
 
-const findBurgerById = async () => {
-  const id = document.getElementById("idburger").value;
+const findByIdBurgers = async () => {
 
-  const response = await fetch(`${baseUrl}/find-burger/${id}`);
+  const id = document.getElementById('idBurger').value;
+ 
+
+  const response = await fetch(`${baseUrl}/find-burgers/${id}`);
 
   const burger = await response.json();
 
-  const burgerEscolhidoDiv = document.getElementById("burgerEscolhido");
+  const burgerEscolhidoDiv = document.getElementById('burgerEscolhido');
 
-  burgerEscolhidoDiv.innerHTML = `<div class="burgerCardItem">
-      <div>
-        <div class="burgerCardItem__nome">${burger.nome}</div>
-        <div class="burgerCardItem__preco">R$ ${burger.preco.toFixed(2)}</div>
-        <div class="burgerCardItem__descricao">${burger.descricao}</div>
-      </div>
-        <img class="burgerCardItem__foto" src=${
-          burger.foto
-        } alt=${`${burger.nome}`} />
-    </div>`;
+
+  burgerEscolhidoDiv.innerHTML = `
+  <div class="BurgerCardItem">
+    <div>
+      <div class="BurgerCardItem__nome">${burger.nome}</div>
+      <div class="BurgerCardItem__descricao">${burger.descricao}</div>
+      <div class="BurgerCardItem__preco">R$ ${burger.preco.toFixed(2)}</div>
+    </div>
+    <img class="BurgerCardItem__foto"
+    src=${burger.foto} 
+    alt=${`${burger.nome}`} />
+  </div>`;
 };
